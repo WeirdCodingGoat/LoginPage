8b597b4257fe3277d64479cba71f6f56365ab525
WeirdCodingGoat <gillionrafeal@gmail.com>
2023-10-06 11:05:57 -0500

    Second commit (Done?).

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
-
 Builder.load_file("QuizPage.kv")
 class QuizPageApp(App):
     def build(self):
@@ -19,12 +18,19 @@ class Question1Screen(Screen):
             self.manager.current = "wrong"
 
 class Question2Screen(Screen):
-    pass
+    def answer_question(self, num):
+        if num == "2012":
+            self.manager.current = "correct"
+        else:
+            self.manager.current = "wrong"
 
 class CorrectScreen(Screen):
     def next(self):
+        self.manager.current = "question2"
 
 class WrongScreen(Screen):
-    pass
+    def next(self):
+        self.manager.current = "question2"
+
 
 QuizPageApp().run()
