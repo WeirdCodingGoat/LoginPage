aead9da9ea07d9a775087beaa92b6e2239e24d6c
WeirdCodingGoat <iggy@Mac-mini.local>
2023-10-23 22:21:02 -0500

    Half of finnishing touches

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 upper_letters=["A","B","C","D","E","F","G","H","I","J","K","L","M","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-
+print("why")
 Builder.load_file("LoginPage.kv")
 class LoginPageApp(App):
     def build(self):
         return PageManager()
-
+print("why")
 class PageManager(ScreenManager):
     pass
-
+print("why")
 def password_requirements(password,upper_letters):
     cap_check=False
     num_check=False
@@ -42,7 +42,7 @@ def password_requirements(password,upper_letters):
             spe_check=True
     check_list=[len_check,num_check,cap_check,low_check,spe_check]
     return check_list
-    
+print("why")
     
 def req_handling(check_list):
     #len_check=index0, num_check=index1, cap_check=index2, low_check=index3, spe_check=inde4
@@ -56,7 +56,7 @@ def req_handling(check_list):
         return -1
     else:
         return code_handle
-            
+print("why")
 class LoginPage(Screen):
     def get_logged_in(self,username,password):
         code_var = req_handling(password_requirements(password,upper_letters))
@@ -64,31 +64,25 @@ class LoginPage(Screen):
 
     def switch_screen(self): #Screen change function(s)?
         self.manager.current = "newaccount"
-
+print("why")
 
 class NewAccountPage(Screen):
-    #req1 = Label(text="You don't have at least 8 characters.")
-    #req2 = Label(text="You don't have at least 1 number.")
-    #req3 = Label(text="You don't have at least 1 uppercase letter.")
-    #req4 = Label(text="You don't have at least 1 lowercase letter.")
-    #req5 = Label(text='You don't have at least 1 "special" character.')
-    #err1 = Label(text="You have a space in your password.")
-    #err2 = Label(text="Make sure you didn't make a new line and they are exactly the same.")
-    def answer_question(self, password, password2):
+   
+    def answer_question(self, password, password2, username):
         if password == password2:
             check_list=password_requirements(password,upper_letters)
             print(req_handling(check_list))
         else:
             print("Make sure they are the same.")
     
-    def place_holder(self): #Screen change function(s)?
-        print("You need a function, put 'root.' here")
-        
+    def switch_screen(self):
+        self.manager.current = "login"
+print("why")
+class HomePage(Screen):
+    def switch_screen(self):
+        self.manager.current = "login"
 
-class CorrectScreen(Screen):
-    def next(self):
-        self.manager.current = "newaccount"
-
+print("why")
 class WrongScreen(Screen):
     def next(self):
         self.manager.current = "newaccount"
