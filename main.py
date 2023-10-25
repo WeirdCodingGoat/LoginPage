3761c3843c98f7307a7564c7943036f81c2570d9
WeirdCodingGoat <iggy@Mac-mini.local>
2023-10-25 00:46:00 -0500

    Finnal commit. (Finnished)

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 upper_letters=["A","B","C","D","E","F","G","H","I","J","K","L","M","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-print("why")
+
 Builder.load_file("LoginPage.kv")
 class LoginPageApp(App):
     def build(self):
         return PageManager()
-print("why")
+
 class PageManager(ScreenManager):
     pass
-print("why")
+
 def password_requirements(password,upper_letters):
     cap_check=False
     num_check=False
@@ -42,7 +42,7 @@ def password_requirements(password,upper_letters):
             spe_check=True
     check_list=[len_check,num_check,cap_check,low_check,spe_check]
     return check_list
-print("why")
+
     
 def req_handling(check_list):
     #len_check=index0, num_check=index1, cap_check=index2, low_check=index3, spe_check=inde4
@@ -56,37 +56,67 @@ def req_handling(check_list):
         return -1
     else:
         return code_handle
-print("why")
+
 class LoginPage(Screen):
-    def get_logged_in(self,username,password):
-        code_var = req_handling(password_requirements(password,upper_letters))
-        print(code_var)
+    def get_logged_in(self,password,username):
+            with open(r"accInfo.txt","r") as accInfo:
+                accounts=accInfo.read()
+                pass_start=accounts.find(username)+len(username)+1
+                pass_end=pass_start+len(password)
+                if username in accounts:
+                    if password == accounts[pass_start:pass_end]:
+                        accInfo.close()
+                        self.manager.current = "home"
+                    else:
+                        accInfo.close()
+                      # self.manager.current = "wrong"
 
     def switch_screen(self): #Screen change function(s)?
         self.manager.current = "newaccount"
-print("why")
+
 
 class NewAccountPage(Screen):
-   
     def answer_question(self, password, password2, username):
+        
+        req_met=0
         if password == password2:
-            check_list=password_requirements(password,upper_letters)
-            print(req_handling(check_list))
+            req_code=req_handling(password_requirements(password,upper_letters))
+            if req_code == -1:
+                self.manager.current = "err2"
+            else:
+                for digit in req_code:
+                    if digit == "1":
+                        req_met += 1
+                if req_met == len(req_code):
+                    with open(r"accInfo.txt","a") as accInfo:
+                        accInfo.write(" "+username+" "+password)
+                        accInfo.close()
+                    self.manager.current = "success"
+                else:
+                    self.manager.current = "err3"
+                    print(req_code,req_met,len(req_code))
         else:
-            print("Make sure they are the same.")
+            self.manager.current = "err1"
     
     def switch_screen(self):
         self.manager.current = "login"
-print("why")
+
 class HomePage(Screen):
     def switch_screen(self):
         self.manager.current = "login"
 
-print("why")
+
 class WrongScreen(Screen):
     def next(self):
         self.manager.current = "newaccount"
-
-
+class SpaceErr(Screen):
+    def next(self):
+        self.manager.current = "newaccount"
+class UnmetRequirement(Screen):
+    def next(self):
+        self.manager.current = "newaccount"
+class AccSuccess(Screen):
+    def next(self):
+        self.manager.current = "login"
 LoginPageApp().run()
 
